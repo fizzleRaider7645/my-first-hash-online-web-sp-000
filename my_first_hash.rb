e47dc3d567bc857aba01676aec27b29f7baaf71c
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-08 14:05:47 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,6 @@
-your_hash = {"bad_pet": "cat", "good_pet": "dog"}
+your_hash = {"bad_pet"=> "cat", "good_pet"=> "dog"}
 
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
 end
 
 
